0df71cf871b1456f035c9d5d9cdac32cc7340234
LotiD <didierloti1@gmail.com>
2025-09-08 23:44:07 +0200

    fix path .env

--- m1-rattrapages-loti-didier/01-Intelligence-Artificielle-pour-le-web/api/server.mjs
+++ m1-rattrapages-loti-didier/01-Intelligence-Artificielle-pour-le-web/api/server.mjs
@@ -2,8 +2,13 @@ import express from "express";
 import cors from "cors";
 import OpenAI from "openai";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 if (!process.env.OPENAI_API_KEY) {
     console.error("Clé API OpenAI manquante !");
